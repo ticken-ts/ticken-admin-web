716f401dadd33e7f235923c5a0bc88fe23ff3701
Javier <javiferreyra97@gmail.com>
2022-10-25 23:24:01 -0300

    Fix layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useSessionStore } from "@/stores/session";
+import IconCommunity from '@/components/icons/IconCommunity.vue';
+import IconDocumentation from '@/components/icons/IconDocumentation.vue';
 
 const session = useSessionStore();
 
@@ -9,8 +11,37 @@ function logout() {
 </script>
 
 <template>
-  <main>
-    <h1>Home page</h1>
-    <button @click="logout">Logout</button>
-  </main>
+  <div class="container">
+    <header>
+      <h2>Dashboard</h2>
+    </header>
+    <main>
+      <h1>Home page</h1>
+      <button @click="logout">Logout</button>
+    </main>
+  </div>
 </template>
+
+<style scoped lang="scss">
+@import "@/assets/colors.scss";
+
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
+
+header {
+  background-color: $primary;
+  width: 100%;
+  color: white;
+  padding: 10px;
+}
+
+main {
+  justify-content: flex-start;
+  align-items: center;
+  max-width: 768px;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1>Login</h1>
-  <input type="email" v-model="email" />
-  <input type="password" v-model="password" />
-  <button @click="session.login">Login</button>
+  <main>
+    <h1>Login</h1>
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="session.login">Login</button>
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -26,6 +28,14 @@ watch(isLoggedIn, (is) => {
 </script>
 
 <style scoped>
+main {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
 input {
   margin: 0.5rem 0;
 }
