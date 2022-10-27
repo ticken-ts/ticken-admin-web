a3368675dcb0e3908a01273be599c83cfda9e365
Javier <javiferreyra97@gmail.com>
2022-10-26 22:00:08 -0300

    Install quasar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { Quasar } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+import "quasar/src/css/index.sass";
 
 import App from "@/App.vue";
 import router from "@/router";
@@ -11,6 +15,9 @@ import "@/assets/styles.scss";
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
 pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
