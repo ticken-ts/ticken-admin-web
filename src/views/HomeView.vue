81f8b5d0adb13660921cd681b2b763e8b89bc88a
Javier <javiferreyra97@gmail.com>
2022-10-27 01:29:20 -0300

    Add basic home layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,65 @@
 <script setup lang="ts">
 import { useSessionStore } from "@/stores/session";
-import IconCommunity from '@/components/icons/IconCommunity.vue';
-import IconDocumentation from '@/components/icons/IconDocumentation.vue';
+import {ref} from 'vue';
+
+const showLogoutPrompt = ref(false);
 
 const session = useSessionStore();
-
 function logout() {
   session.logout();
 }
+
+function openLogoutPrompt() {
+  showLogoutPrompt.value = true;
+}
+
 </script>
 
 <template>
-  <div class="container">
-    <header>
-      <h2>Dashboard</h2>
-    </header>
-    <main>
-      <h1>Home page</h1>
-      <button @click="logout">Logout</button>
-    </main>
-  </div>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title class="justify-start flex items-center">
+          <q-avatar>
+            <q-icon name="dashboard"/>
+          </q-avatar>
+          Dashboard
+          <q-space></q-space>
+          <q-btn flat icon="logout" @click="openLogoutPrompt"/>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="row justify-md-center justify-evenly">
+      <div class="col-md-5 col-11">
+        <q-card class="q-col-gutter">
+          <q-card-section>
+            <h4>Organization</h4>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-md-5 col-11">
+        <q-card class="q-gutter">
+          <q-card-section>
+            <h4>Events</h4>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-page-container>
+    <q-dialog persistent transition-show="scale" v-model="showLogoutPrompt">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Are you sure you want to logout?</div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" @click="showLogoutPrompt = false"/>
+          <q-btn flat label="Logout" color="primary" @click="logout"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+  </q-layout>
 </template>
 
 <style scoped lang="scss">
