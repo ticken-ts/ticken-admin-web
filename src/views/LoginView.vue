28df57519b64915ca9217f317a12cba40a227f9b
Javier <javiferreyra97@gmail.com>
2022-10-29 01:00:49 -0300

    Improve login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,15 @@
 <template>
   <main>
-    <h1>Login</h1>
-    <input type="email" v-model="email" />
-    <input type="password" v-model="password" />
-    <button @click="session.login">Login</button>
+    <img src="@/assets/bg-login.jpg" alt="background" class="background">
+    <q-card>
+      <q-card-section class="column items-center">
+        <img :src="logo" alt="logo" class="logo" />
+        <h4 class="title">Login</h4>
+        <input placeholder="email" type="email" v-model="email" />
+        <input placeholder="password" type="password" v-model="password" />
+        <button @click="session.login">Login</button>
+      </q-card-section>
+    </q-card>
   </main>
 </template>
 
@@ -12,6 +18,7 @@ import { ref, watch } from "vue";
 import { useSessionStore } from "@/stores/session";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
+import logo from "@/assets/logo.svg";
 
 const email = ref("");
 const password = ref("");
@@ -27,7 +34,8 @@ watch(isLoggedIn, (is) => {
 });
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "@/assets/colors.scss";
 main {
   display: flex;
   height: 100%;
@@ -41,5 +49,22 @@ input {
 }
 button {
   margin-top: 1rem;
+}
+
+.logo {
+  height: 25vh;
+  pointer-events: none;
+}
+
+.title {
+  margin-bottom: 1em;
+}
+
+.background {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  background-color: $primary-dark;
 }
 </style>
